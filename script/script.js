eeee8cc1b88fa5ccbd14ad851f8fa8437295317a
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2023-07-20 19:39:48 +0300

    modified

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
 var toggler = document.querySelector(".toggle"),
     nav = document.querySelector(".responsive_nav"),
     close = document.querySelector(".close")
+    // progressSection = document.querySelector("#skills"),
+    // progress = document.querySelectorAll("progress")
 toggler.onclick = function(){
     nav.classList.add("open_close_tab")
 }
 close.onclick = function(){
     nav.classList.remove("open_close_tab")
 }
+
+
+// window.onscroll = function(){
+//     if(window.scrollY >= progressSection.offsetTop - 150){
+//         console.log("reached")
+//         progress.forEach((progress) => {
+//             progress.value = progress.dataset.value;
+//         })
+//     }
+// }
